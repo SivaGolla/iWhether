c5ffc7cc702e9dfe4dc23e87e6b68d40a399ad46
Siva Golla <gvsivannarayana@gmail.com>
2024-10-01 22:18:04 +0100

    Initial project files

---
+++ iWhether/iWhetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWhetherApp.swift
+//  iWhether
+//
+//  Created by Venkata Sivannarayana Golla on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct iWhetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
